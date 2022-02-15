db1e60bbcdfdc82c6ec8174aba00d938b5cd3694
Lucas Christianson <luchrist@nmu.edu>
2022-02-15 15:57:37 -0500

    Create WordWrapper.java
    Creation of WordWrapper class.  Added brief summary of the class and included required method "wrap".

---
+++ WordWrapper.java
@@ -0,0 +1,11 @@
+//Author: Lucas Christianson
+
+//This is a class that takes a string and inserts line breaks at a given interval to achieve a simple type of word wrap.
+
+
+
+public class WordWrapper {
+    public static String wrap(String text, int maxLineLength) {
+        return "";
+    }
+}
